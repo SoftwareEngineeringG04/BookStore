61a09359aa9ee7cddbc584f9afeffb425b5f3252
sevket cerit <cerits@itu.edu.tr>
2017-11-20 22:04:32 +0300

    init project commit

---
+++ 1773BookStore/1773BookStore/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(_1773BookStore.Startup))]
+namespace _1773BookStore
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
